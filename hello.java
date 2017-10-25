e8a65a260bf3af87563cca4c1ae517767f2f0a69
cafierok <whoaiskeith@gmail.com>
2017-10-25 13:05:12 -0400

    added scanner functionality

--- hello.java
+++ hello.java
@@ -1,13 +1,20 @@
 // a simple program to practice common Github commands 
 
 public class hello {
+	import java.util.Scanner;
 
     public static void main(String [] args) {
 
 
 	// added  line	
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Enter your name");
+		String name = s.nextLine();
+		
+		System.out.println("Hello " + name + ", my name is Keith")
 
-        System.out.println("Hello Class!");
+        //System.out.println("Hello User");
 
 
 
